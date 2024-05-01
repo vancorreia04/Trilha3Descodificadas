19f3d09ee2916574bfbf077289bd3b7a86a677dc
vancorreia04 <109099486+vancorreia04@users.noreply.github.com>
2024-05-01 15:58:06 -0300

    Add files via upload

---
+++ TreinoJavaScriptLogica/Aulas/ex08.js
@@ -0,0 +1,5 @@
+var vel = `78.2`;
+console.log(`A velocidade do seu carro é ${vel}Km\h`);
+if(vel > 60){console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirige sempre usando cinto de segurança!`);
---
+++ TreinoJavaScriptLogica/Aulas/ex09.js
@@ -0,0 +1,6 @@
+var pais =`França`;
+if(pais ==`França`){
+    console.log(`Vivendo em ${pais}`);
+}else if((pais=`Brasil`)){
+    console.log(`Vivendo em ${pais}`);
+}
